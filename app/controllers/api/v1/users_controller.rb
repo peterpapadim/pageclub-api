24b801a3d5e17345408aa7c46457729a0e1cd748
Peter <ppapadim12@gmail.com>
2017-09-29 17:52:06 -0400

    update and destroy actions created for a user. Users can now edit and delete their account from the client

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -22,13 +22,27 @@ class Api::V1::UsersController < ApplicationController
     token = request.headers["HTTP_AUTHORIZATION"].split.last
     user_id = Auth.decode(token)
     user = User.find(user_id["user"])
-    render json: {id: user.id, firstName: user.first_name, lastName: user.last_name, email: user.email}
+    render json: {id: user.id, firstName: user.first_name, lastName: user.last_name, email: user.email, memberSince: user.created_at}
+  end
+
+  def update
+    user = User.find(params[:id])
+    updated_first_name = params[:updated_full_name].split.first
+    updated_last_name = params[:updated_full_name].split.last
+    user.update(first_name: updated_first_name, last_name: updated_last_name)
+    render json: {id: user.id, firstName: user.first_name, lastName: user.last_name, email: user.email, memberSince: user.created_at}
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: {}
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :id)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :id, :updated_full_name)
   end
 
 end
